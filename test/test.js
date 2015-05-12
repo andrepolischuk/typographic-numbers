18efb06cd96880c900cf9069de8cc9223c448e4b
andrepolischuk <andre.polischuk@gmail.com>
2015-05-12 11:04:27 +0300

    Update test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 
+'use strict';
+
 var numbers = require('..');
 var assert = require('assert');
 
@@ -18,7 +20,7 @@ describe('numbers(input)', function() {
   it('should fix multiple numbers', function() {
     assert(numbers('215 and 215.215') === '215 and 215.215');
     assert(numbers('15215 and 15215,215') === '15,215 and 15,215.215');
-    assert(numbers('9015215 and 9015215.215') === '9,015,215 and 9,015,215.215');
+    assert(numbers('9015215 and 9015215.21') === '9,015,215 and 9,015,215.21');
   });
 
   it('should return not changed string for undefined locale', function() {
