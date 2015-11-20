d7467b55a7ba75583c199f94d68556a1313c7ba7
andrepolischuk <andre.polischuk@gmail.com>
2015-11-21 02:45:04 +0300

    Fix #2 formatting card numbers

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 var locales = require('./locales');
+var americanExpress = '3[47]\\d\\d(?:\\s\\d{4}){2}\\s\\d{3}';
+var maestro = '(?:5[0678]\\d\\d|6304|6390|67\\d\\d)(?:\\s\\d{4}){2}(?:\\s\\d{1,7})?';
+var mastercard = '5[1-5]\\d\\d(?:\\s\\d{4}){3}';
+var visa = '4\\d{3}(?:\\s\\d{4}){2}\\s\\d(?:\\d{3})?';
+var number = '\\d+';
+
+var numberRegExp = new RegExp('(' + [
+  americanExpress,
+  maestro,
+  mastercard,
+  visa,
+  number
+].join('|') + ')(?:[.,](\\d+))?', 'gm');
 
 module.exports = function (input, opts) {
   opts = opts || {};
@@ -7,7 +20,8 @@ module.exports = function (input, opts) {
   var sep = locales[locale.toLowerCase()];
   if (!sep) return input;
 
-  return input.replace(/(\d+)[\.,]*(\d*)/gim, function (match, num, dec) {
+  return input.replace(numberRegExp, function (match, num, dec) {
+    if (num.indexOf(' ') > 0) return match;
     return format(+num, sep.charAt(0)) + (dec ? sep.charAt(1) + dec : '');
   });
 };
--- test.js
+++ test.js
@@ -2,32 +2,35 @@
 var numbers = require('./');
 var assert = require('assert');
 
-describe('numbers(input)', function () {
-  it('should fix integer', function () {
-    assert(numbers('215') === '215');
-    assert(numbers('15215') === '15,215');
-    assert(numbers('9015215') === '9,015,215');
-  });
+it('should fix integer', function () {
+  assert(numbers('215') === '215');
+  assert(numbers('15215') === '15,215');
+  assert(numbers('9015215') === '9,015,215');
+});
 
-  it('should fix float', function () {
-    assert(numbers('215.215') === '215.215');
-    assert(numbers('15215,215') === '15,215.215');
-    assert(numbers('9015215.215') === '9,015,215.215');
-  });
+it('should fix float', function () {
+  assert(numbers('215.215') === '215.215');
+  assert(numbers('15215,215') === '15,215.215');
+  assert(numbers('9015215.215') === '9,015,215.215');
+});
 
-  it('should fix multiple numbers', function () {
-    assert(numbers('215 and 215.215') === '215 and 215.215');
-    assert(numbers('15215 and 15215,215') === '15,215 and 15,215.215');
-    assert(numbers('9015215 and 9015215.21') === '9,015,215 and 9,015,215.21');
-  });
+it('should fix multiple numbers', function () {
+  assert(numbers('215 and 215.215') === '215 and 215.215');
+  assert(numbers('15215 and 15215,215') === '15,215 and 15,215.215');
+  assert(numbers('9015215 and 9015215.21') === '9,015,215 and 9,015,215.21');
+});
 
-  it('should return not changed string for undefined locale', function () {
-    assert(numbers('215.215', {locale: 'rs-ru'}) === '215.215');
-    assert(numbers('15215,215', {locale: 'rs-ru'}) === '15215,215');
-  });
+it('should return origin for credit card number', function () {
+  assert(numbers('5211 8814 8501 7922') === '5211 8814 8501 7922');
+  assert(numbers('15215,215 from 4211 8814 8501 7922') === '15,215.215 from 4211 8814 8501 7922');
+});
 
-  it('should fix numbers via case-insensitive locale', function () {
-    assert(numbers('215.215', {locale: 'ru-RU'}) === '215,215');
-    assert(numbers('15215,215', {locale: 'ru-ru'}) === '15 215,215');
-  });
+it('should return origin for undefined locale', function () {
+  assert(numbers('215.215', {locale: 'rs-ru'}) === '215.215');
+  assert(numbers('15215,215', {locale: 'rs-ru'}) === '15215,215');
+});
+
+it('should fix numbers via case-insensitive locale', function () {
+  assert(numbers('215.215', {locale: 'ru-RU'}) === '215,215');
+  assert(numbers('15215,215', {locale: 'ru-ru'}) === '15 215,215');
 });
