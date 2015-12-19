9c4ac55c54f822bfea7fdfdfc330c674de362307
andrepolischuk <andre.polischuk@gmail.com>
2015-12-19 22:30:38 +0300

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,36 +1,35 @@
-'use strict';
-var test = require('ava');
-var numbers = require('./');
+import test from 'ava';
+import numbers from './';
 
-test('should fix integer', function (t) {
+test('fix integer', t => {
   t.is(numbers('215'), '215');
   t.is(numbers('15215'), '15,215');
   t.is(numbers('9015215'), '9,015,215');
 });
 
-test('should fix float', function (t) {
+test('fix float', t => {
   t.is(numbers('215.215'), '215.215');
   t.is(numbers('15215,215'), '15,215.215');
   t.is(numbers('9015215.215'), '9,015,215.215');
 });
 
-test('should fix multiple numbers', function (t) {
+test('fix multiple numbers', t => {
   t.is(numbers('215 and 215.215'), '215 and 215.215');
   t.is(numbers('15215 and 15215,215'), '15,215 and 15,215.215');
   t.is(numbers('9015215 and 9015215.21'), '9,015,215 and 9,015,215.21');
 });
 
-test('should return origin for credit card number', function (t) {
+test('return origin for credit card number', t => {
   t.is(numbers('5211 8814 8501 7922'), '5211 8814 8501 7922');
   t.is(numbers('15215,215 from 4211 8814 8501 7922'), '15,215.215 from 4211 8814 8501 7922');
 });
 
-test('should return origin for undefined locale', function (t) {
+test('return origin for undefined locale', t => {
   t.is(numbers('215.215', {locale: 'rs-ru'}), '215.215');
   t.is(numbers('15215,215', {locale: 'rs-ru'}), '15215,215');
 });
 
-test('should fix numbers via case-insensitive locale', function (t) {
+test('fix numbers via case-insensitive locale', t => {
   t.is(numbers('215.215', {locale: 'ru-RU'}), '215,215');
   t.is(numbers('15215,215', {locale: 'ru-ru'}), '15 215,215');
 });
